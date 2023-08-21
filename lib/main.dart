fbcc7d076a6f041f4c3f941ba92e185c039cc115
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-21 19:46:16 +0900

    docs: state info

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:example_app/screen/myhome_page.dart';
+import 'package:example_app/screen/state_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const StatePage(),
     );
   }
 }
